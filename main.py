f670ef5f4c7493df59bf6ace307be807fb3471e1
Osama Said Al Zakwani <Osama.Zakwani@gmail.com>
2025-12-08 11:39:33 +0400

    Added the get from API

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import requests
+
 def main():
-    print("Hello from api-test!")
+    response = requests.get("https://jsonplaceholder.typicode.com/todos/2")
+    response_dict = response.json()
+    print (f"Title: {response_dict["title"]}")
+    print (f"Completed: {response_dict["completed"]}")
+    print (f"User Id: {response_dict["userId"]}")
 
 
 if __name__ == "__main__":
