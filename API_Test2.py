8fabc05ac6e3520319fcc9a64b2f1e2221c7648b
Osama Said Al Zakwani <Osama.Zakwani@gmail.com>
2025-12-08 16:32:51 +0400

    Done with addig the full country from the code

--- API_Test2.py
+++ API_Test2.py
@@ -1,15 +1,17 @@
-import requests
+import requests as req
+import pycountry as pyco
 
 name = input("Enter the Person's Name: ")
 
-response = requests.get(f"https://api.nationalize.io/?name={name}")
+response = req.get(f"https://api.nationalize.io/?name={name}")
 
 result_json = response.json()
 
-county_list = result_json["country"]
+country_list = result_json["country"]
 
 print(result_json["name"])
 
-for contries in county_list:
-    print(contries["country_id"])
-
+for countries in country_list:
+    country_code = countries['country_id']
+    full_country = pyco.countries.get(alpha_2=country_code)
+    print(f"countries: {country_code} - {full_country.name}")
